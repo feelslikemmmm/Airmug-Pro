a0931a1125bd5caf518399d6a2db767cf46499c4
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-02-27 19:11:27 +0900

    current scene select js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 (() => {
 
+  let yOffset = 0; //window.pageYOffset 대신 쓸 변수
+  let prevScrollHeight = 0; //현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화 된 눈앞에 보고 있는 scene 또는 scroll-section
   const sceneInfo = [
     {
       //0
@@ -45,9 +48,29 @@
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-    console.log(sceneInfo)
+  }
+
+  const scrollLoop = () => {
+    prevScrollHeight = 0;
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      currentScene--;
+    }
+
   }
 
   window.addEventListener('resize', setLayout);
+  window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset;
+    scrollLoop();
+  });
   setLayout();
 })();
