e664fb7ae6df02da1ea154a10ac8ef0c223d6f9b
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-03-03 17:54:10 +0900

    폰 버그 수정

--- js/main.js
+++ js/main.js
@@ -529,11 +529,13 @@
     window.addEventListener('resize', () => {
       if (window.innerWidth > 900) {
         setLayout();
+        sceneInfo[3].values.rectStartY = 0;
       }
-      sceneInfo[3].values.rectStartY = 0;
     });
 
-    window.addEventListener('orientationchange', setLayout);
+    window.addEventListener('orientationchange', () => {
+      setTimeout(setLayout, 500);
+    });
 
     document.querySelector('.loading').addEventListener('transitionend', (e) => {
       document.body.removeChild(e.currentTarget);
