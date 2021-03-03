d4718b3ca0f602af6998296bcacf0b07f87fdaae
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-03-03 14:48:54 +0900

    스크롤에 따른 캔버스 포지션 변경

--- js/main.js
+++ js/main.js
@@ -340,6 +340,7 @@
         break;
 
       case 3:
+        let step = 0;
         //가로 세로 모두 꽉 차게 하기 위해 여기서 세팅(계산 필요)
         const widthRatio = window.innerWidth / objs.canvas.width;
         const heightRatio = window.innerHeight / objs.canvas.height;
@@ -393,6 +394,17 @@
           objs.canvas.height
         );
 
+        if (scrollRatio < values.rect1X[2].end) {
+          step = 1;
+          console.log('캔버스 닿기 전');
+          objs.canvas.classList.remove('sticky');
+        } else {
+          step = 2;
+          //이미지 블렌드
+          console.log('캔버스 닿은 후');
+          objs.canvas.classList.add('sticky');
+          objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
+        }
 
         break;
 
