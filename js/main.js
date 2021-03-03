c86218ab32bc14ace7abfe843a1d93eba518dc11
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-03-03 15:32:38 +0900

    이미지 블렌딩 2

--- js/main.js
+++ js/main.js
@@ -108,6 +108,7 @@
       values: {
         rect1X: [0, 0, { start: 0, end: 0 }],
         rect2X: [0, 0, { start: 0, end: 0 }],
+        blendHeight: [0, 0, { start: 0, end: 0 }],
         rectStartY: 0
       }
     },
@@ -396,12 +397,22 @@
 
         if (scrollRatio < values.rect1X[2].end) {
           step = 1;
-          console.log('캔버스 닿기 전');
           objs.canvas.classList.remove('sticky');
         } else {
           step = 2;
           //이미지 블렌드
-          console.log('캔버스 닿은 후');
+          values.blendHeight[0] = 0;
+          values.blendHeight[1] = objs.canvas.height;
+          values.blendHeight[2].start = values.rect1X[2].end;
+          values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+
+          const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+          objs.context.drawImage(objs.images[1],
+            0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight,
+            0, objs.canvas.height - blendHeight, objs.canvas.width, blendHeight
+          );
+
           objs.canvas.classList.add('sticky');
           objs.canvas.style.top = `${-(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2}px`;
         }
