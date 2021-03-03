3253d3ada8cc1ca9b003b8d48f2e01aff1771856
feelslikemmmm <67893516+feelslikemmmm@users.noreply.github.com>
2021-03-03 17:42:37 +0900

    초기화 시점 수정

--- js/main.js
+++ js/main.js
@@ -509,34 +509,37 @@
     }
   }
 
-  window.addEventListener('scroll', () => {
-    yOffset = window.pageYOffset;
-    scrollLoop();
-    checkMenu();
-
-    if (!rafState) {
-      rafId = requestAnimationFrame(loop);
-      rafState = true;
-    }
-  });
 
   window.addEventListener('load', () => {
     document.body.classList.remove('before-load');
     setLayout();
     sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0);
+
+    window.addEventListener('scroll', () => {
+      yOffset = window.pageYOffset;
+      scrollLoop();
+      checkMenu();
+
+      if (!rafState) {
+        rafId = requestAnimationFrame(loop);
+        rafState = true;
+      }
+    });
+
+    window.addEventListener('resize', () => {
+      if (window.innerWidth > 900) {
+        setLayout();
+      }
+      sceneInfo[3].values.rectStartY = 0;
+    });
+
+    window.addEventListener('orientationchange', setLayout);
+
+    document.querySelector('.loading').addEventListener('transitionend', (e) => {
+      document.body.removeChild(e.currentTarget);
+    });
   });
 
-  window.addEventListener('resize', () => {
-    if (window.innerWidth > 900) {
-      setLayout();
-    }
-    sceneInfo[3].values.rectStartY = 0;
-  });
-
-  window.addEventListener('orientationchange', setLayout);
-  document.querySelector('.loading').addEventListener('transitionend', (e) => {
-    document.body.removeChild(e.currentTarget);
-  });
   setCanvasImages();
 
 })();
